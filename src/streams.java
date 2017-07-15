9df7ba06b3e511a6e017f43b4437a0aa5480607f
Alex Lin <alexclin@umich.edu>
2017-07-15 18:53:48 -0400

    Add byte array conversion

--- src/streams.java
+++ src/streams.java
@@ -6,16 +6,38 @@ import java.io.*;
 import org.apache.commons.io.*;
 public class streams {
 
-
-
     public static void main(String[] args) throws IOException {
-        File initialFile = new File("filestreamTest.txt");
-        InputStream targetStream = FileUtils.openInputStream(initialFile);
-        File outputFile = new File("output.txt");
-        OutputStream output = FileUtils.openOutputStream(outputFile);
-        IOUtils.copy(targetStream,output);
+//        File initialFile = new File("filestreamTest.txt");
+//        InputStream targetStream = FileUtils.openInputStream(initialFile);
+//
+//        File outputFile = new File("output.txt");
+//        OutputStream output = FileUtils.openOutputStream(outputFile);
+//        IOUtils.copy(targetStream,output);
 
+        File inputFile = new File("filestreamTest.txt");
+        InputStream targetStream = FileUtils.openInputStream(inputFile);
+
+        printByteArrayToFile("fsTestOutput.txt", convertToByteArray(targetStream));
     }
+
+    public static byte[] convertToByteArray(InputStream stream) {
+        byte[] b = null;
+        try {
+            b = IOUtils.toByteArray(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return b;
+    }
+
+    public static void printByteArrayToFile(String pathname, byte[] b) {
+        try {
+            FileUtils.writeByteArrayToFile(new File(pathname), b);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
