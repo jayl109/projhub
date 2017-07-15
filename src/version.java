b23862012cc10c5bfde23d1b1318dc826278c894
jason lum <jasonlum109@berkeley.edu>
2017-07-15 14:03:20 -0700

    ll skeleton

---
+++ src/version.java
@@ -0,0 +1,12 @@
+/**
+ * Created by Jason on 7/15/17.
+ */
+public class version {
+    int versionNum;
+    //some file
+    public version(int v) {
+        versionNum = v;
+        //some file
+
+    }
+}
---
+++ src/versionList.java
@@ -0,0 +1,18 @@
+/**
+ * Created by Jason on 7/15/17.
+ */
+import java.util.LinkedList;
+public class versionList {
+    public int id;
+    private LinkedNode start;
+    private LinkedNode head;
+    private class LinkedNode {
+        private LinkedNode() {
+            head = new LinkedList<>();
+            start = head;
+        }
+
+    }
+
+
+}
