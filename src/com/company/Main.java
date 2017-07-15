cfb3e3bc9e15f8212c95cfc83a83de15ec7ec804
Alex Lin <alexclin@umich.edu>
2017-07-15 17:34:17 -0400

    Skeleton for arguments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("test");
+        if (args.length <= 0) {
+            // Error
+            System.out.println("Usage message");
+        } else {
+            parseArgs(args);
+        }
+    }
+
+    private static void parseArgs(String[] args) {
+        String cmd = args.length >= 1 ? args[0] : null;
+        String cmdarg1 = args.length >= 2 ? args[1] : null;
+        String cmdarg2 = args.length >= 3 ? args[2] : null;
+
+        switch (cmd) {
+            case "init":
+                // init
+                System.out.println("Command is 'init' ");
+                break;
+            default:
+                // Command doesn't exist
+                System.out.println("Command doesn't exist");
+        }
     }
 }
