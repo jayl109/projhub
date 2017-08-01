c1e0dc5416d90dfd6139e250e61e75dd60e49d7f
jason lum <jasonlum109@berkeley.edu>
2017-07-31 19:04:28 -0700

    idr what i changed

--- src/com/company/Commit.java
+++ src/com/company/Commit.java
@@ -9,36 +9,7 @@ import java.util.Date;
  *
  */
 
-/*
- class Commit implements Serializable:
-    // Constructor
-    public Commit(String parentId, String message)
 
-    // Fields
-    /** The SHA-1 identifier of my parent, or null if I am the initial commit.
-    private final String parent;
-
-    /** My log message.
-    private final String message;
-
-    /** My timestamp. (java.util.Date)
-    private Date commitDate;
-
-    /** A mapping of file names to the SHA-1's of their blobs.
-    private HashMap<String, String> contents = new HashMap<>();
-
-    // Methods
-    public String toString()
-
-    /** Get SHA-1 identifier of my parent, or null if I am the initial commit.
-    String getParent()
-
-    /** Finalize me and write me to my repository.
-    void finalize()
-
-    /* Assorted getters and setters for contents and other fields excluded.
-    Methods supporting merge also excluded.
- */
 public class Commit implements Serializable {
 
     private final String parent;
--- src/com/company/Utils.java
+++ src/com/company/Utils.java
@@ -1,5 +1,6 @@
 package gitlet;
 
+//figure out what we can delete
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
@@ -11,17 +12,17 @@ import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
 import java.util.Formatter;
 import java.util.List;
-
+package com.company;
 
 /** Assorted utilities.
  *  @author P. N. Hilfinger
  */
 class Utils {
 
-    /* SHA-1 HASH VALUES. */
-
-    /** Returns the SHA-1 hash of the concatenation of VALS, which may
-     *  be any mixture of byte arrays and Strings. */
+    /**
+     * Returns the SHA-1 hash of the concatenation of VALS, which may
+     * be any mixture of byte arrays and Strings.
+     */
     static String sha1(Object... vals) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-1");
@@ -44,18 +45,22 @@ class Utils {
         }
     }
 
-    /** Returns the SHA-1 hash of the concatenation of the strings in
-     *  VALS. */
+    /**
+     * Returns the SHA-1 hash of the concatenation of the strings in
+     * VALS.
+     */
     static String sha1(List<Object> vals) {
         return sha1(vals.toArray(new Object[vals.size()]));
     }
 
     /* FILE DELETION */
 
-    /** Deletes FILE if it exists and is not a directory.  Returns true
-     *  if FILE was deleted, and false otherwise.  Refuses to delete FILE
-     *  and throws IllegalArgumentException unless the directory designated by
-     *  FILE also contains a directory named .gitlet. */
+    /**
+     * Deletes FILE if it exists and is not a directory.  Returns true
+     * if FILE was deleted, and false otherwise.  Refuses to delete FILE
+     * and throws IllegalArgumentException unless the directory designated by
+     * FILE also contains a directory named .gitlet.
+     */
     static boolean restrictedDelete(File file) {
         if (!(new File(file.getParentFile(), ".gitlet")).isDirectory()) {
             throw new IllegalArgumentException("not .gitlet working directory");
@@ -65,91 +70,6 @@ class Utils {
         } else {
             return false;
         }
-    }
-
-    /** Deletes the file named FILE if it exists and is not a directory.
-     *  Returns true if FILE was deleted, and false otherwise.  Refuses
-     *  to delete FILE and throws IllegalArgumentException unless the
-     *  directory designated by FILE also contains a directory named .gitlet. */
-    static boolean restrictedDelete(String file) {
-        return restrictedDelete(new File(file));
-    }
-
-    /* READING AND WRITING FILE CONTENTS */
-
-    /** Return the entire contents of FILE as a byte array.  FILE must
-     *  be a normal file.  Throws IllegalArgumentException
-     *  in case of problems. */
-    static byte[] readContents(File file) {
-        if (!file.isFile()) {
-            throw new IllegalArgumentException("must be a normal file");
-        }
-        try {
-            return Files.readAllBytes(file.toPath());
-        } catch (IOException excp) {
-            throw new IllegalArgumentException(excp.getMessage());
-        }
-    }
-
-    /** Write the entire contents of BYTES to FILE, creating or overwriting
-     *  it as needed.  Throws IllegalArgumentException in case of problems. */
-    static void writeContents(File file, byte[] bytes) {
-        try {
-            if (file.isDirectory()) {
-                throw
-                    new IllegalArgumentException("cannot overwrite directory");
-            }
-            Files.write(file.toPath(), bytes);
-        } catch (IOException excp) {
-            throw new IllegalArgumentException(excp.getMessage());
-        }
-    }
-
-    /* OTHER FILE UTILITIES */
-
-    /** Return the concatentation of FIRST and OTHERS into a File designator,
-     *  analogous to the {@link java.nio.file.Paths.#get(String, String[])}
-     *  method. */
-    static File join(String first, String... others) {
-        return Paths.get(first, others).toFile();
-    }
-
-    /** Return the concatentation of FIRST and OTHERS into a File designator,
-     *  analogous to the {@link java.nio.file.Paths.#get(String, String[])}
-     *  method. */
-    static File join(File first, String... others) {
-        return Paths.get(first.getPath(), others).toFile();
-    }
-
-    /* DIRECTORIES */
-
-    /** Filter out all but plain files. */
-    private static final FilenameFilter PLAIN_FILES =
-        new FilenameFilter() {
-            @Override
-            public boolean accept(File dir, String name) {
-                return new File(dir, name).isFile();
-            }
-        };
-
-    /** Returns a list of the names of all plain files in the directory DIR, in
-     *  lexicographic order as Java Strings.  Returns null if DIR does
-     *  not denote a directory. */
-    static List<String> plainFilenamesIn(File dir) {
-        String[] files = dir.list(PLAIN_FILES);
-        if (files == null) {
-            return null;
-        } else {
-            Arrays.sort(files);
-            return Arrays.asList(files);
-        }
-    }
-
-    /** Returns a list of the names of all plain files in the directory DIR, in
-     *  lexicographic order as Java Strings.  Returns null if DIR does
-     *  not denote a directory. */
-    static List<String> plainFilenamesIn(String dir) {
-        return plainFilenamesIn(new File(dir));
     }
 
 }
