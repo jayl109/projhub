3058bc7fcbfc8b0da142e49e27ae1d77c1f2258d
Alex Lin <alexclin@umich.edu>
2017-08-01 20:10:12 -0400

    Staging

--- src/com/company/Stage.java
+++ src/com/company/Stage.java
@@ -1,25 +1,43 @@
+import java.io.File;
+import java.io.Serializable;
+import java.util.Collections;
+import java.util.HashSet;
+
 /**
  * Created by Jason on 7/15/17.
  */
 /*
  */
 
-public class Stage {
-    ArrayList<File> files;
+public class Stage implements Serializable {
+    HashSet<File> files;
     String filepath;
+
     public Stage() {
-        filepath = = System.getProperty("user.dir")+"/.projhub/staged";
+        filepath = System.getProperty("user.dir")+"/.projhub/staged";
     }
+
     //add filepaths to staging area
     public boolean add(String[] filepaths) {
-        return false;
+        for (String filepath : filepaths) {
+            files.add(new File(filepath));
+        }
+        return true;
     }
     //remove filepaths from staging area;
     public boolean remove(String[] filepaths) {
-        return false;
+        for (String filepath : filepaths) {
+            files.remove(new File(filepath));
+        }
+        return true;
     }
+
     //clears staging area after commit
     public void clear() {
-        files = new ArrayList<File>;
+        files = new HashSet<>();
+    }
+
+    public HashSet<File> getFiles() {
+        return files;
     }
 }
